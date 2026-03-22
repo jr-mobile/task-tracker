96c30b67429d565aaa14517e9cd1bb17f5770209
Jon <jrmobile2002@gmail.com>
2026-03-22 21:11:22 +0000

    Updated Docker

--- task-tracker/frontend/vite.config.ts
+++ task-tracker/frontend/vite.config.ts
@@ -5,10 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    host: true,
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://backend:3001',
         changeOrigin: true
       }
     }
